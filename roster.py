cf7b44afcb1951c5f545be67f0e1e4d5cb9c3945
ken-xiong <kenxiong@unc.edu>
2025-06-13 23:15:53 -0400

    Removing Roster Var

--- roster.py
+++ roster.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 
-roster = ['Claude', 'Brown', 'Davis']
-player = {"Last Name": roster,
+player = {"Last Name": ['Claude', 'Brown', 'Davis'],
           "First Name": ['Ty', 'James', 'RJ'],
           "height": [79, 82, 72],
-          "weight": [230,215,180]}
+          "weight": [230,215,180]
+          }
 data = pd.DataFrame(player)
 print(data)
